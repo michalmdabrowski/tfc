70a3df39a78c8e0fbad7d7bbc525ae1f7fa55434
Michał Dąbrowski <michalpietryka@outlook.com>
2023-02-02 13:58:29 +0100

    add comments to controller methods

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -16,6 +16,7 @@ class IssueController extends Controller
     {
         $this->authorize('viewAny', Issue::class);
 
+        // To oczywiście możemy przenieść do jakiegoś Repository, ale byłby to przerost formy nad treścią
         $issues = Issue::when($request->get('status'), function($query, $status) {
             return $query->withoutGlobalScope(DefaultStatuesScope::class)->where('status', $status);
         })->get();
@@ -24,12 +25,14 @@ class IssueController extends Controller
 
     public function create(CreateIssueRequest $request)
     {
+        // Zarówno create jak i update można opakować w Command i dodać do kolejki, ale przy banalnych aplikacjach to bez sensu
         $issue = Issue::create($request->validated());
         return new IssueResource($issue);
     }
 
     public function update(UpdateIssueRequest $request, Issue $issue)
     {
+        // Patrz wyżej, dodatkowo można tutaj wrzucić jakiś setter na to w modelu, gdyby były jakieś reguły odnośnie tego jaki stan na jaki można zmieniać
         $issue->status = $request->status;
         $issue->save();
         return new IssueResource($issue);
